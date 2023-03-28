b44df2ae67c07f74ac9ae8d8b88666a2ca47fba3
Oğuz Can <107717374+Eraxus1@users.noreply.github.com>
2023-03-28 17:16:41 +0300

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>Welcome to my about page!</p>
+    <AnotherComponent />
+  </div>
+</template>
+
+
+<script>
+import AnotherComponent from "@/components/AnotherComponent.vue";
+
+export default {
+  components: {
+    AnotherComponent,
+  },
+};
+</script>
